ea4f2da5ba3cd7ff34da992c213b86bf13a092a8
rathor1995 <adityarathor270395@gmail.com>
2021-06-12 13:35:37 +0530

    Headless

---
+++ src/main/java/BaseClass.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseClass {
+    static WebDriver driver;
+
+    public static void BrowserOpen()
+    {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");
+        driver = new ChromeDriver(options);
+        // driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://mail.google.com/");
+
+    }
+
+
+}
---
+++ src/main/java/login_Gmail.java
@@ -0,0 +1,36 @@
+import org.apache.batik.css.engine.sac.CSSSelectorFactory;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.By;
+
+import java.io.IOException;
+
+public class login_Gmail {
+    @FindBy(xpath="//input[@type='email']") private WebElement id;
+    //  @FindBy(css="div.VfPpkd-RLmnJb") private WebElement next1;
+    @FindBy(xpath="//span[@jsname='V67aGc']") private WebElement next1;
+    @FindBy(xpath="//input[@name='password']") private WebElement password;
+    @FindBy(xpath="//span[text()='Next']") private WebElement next2;
+    public login_Gmail(WebDriver driver)
+    {
+        PageFactory.initElements(driver,this);
+    }
+
+    public void Login() throws InvalidFormatException, IOException, InterruptedException
+    {   Thread.sleep(3000);
+        id.sendKeys(Utility_Class.getTD(0, 0));
+        Thread.sleep(3000);
+        next1.click();
+        Thread.sleep(3000);
+        password.sendKeys(Utility_Class.getTD(0,1));
+        Thread.sleep(3000);
+        next2.click();
+        Thread.sleep(3000);
+
+    }
+
+
+}
